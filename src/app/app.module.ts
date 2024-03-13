b0eeb71899430b1804d41f0f6696653f89e176fe
Hayley <60548521+HFairbanks01@users.noreply.github.com>
2024-03-13 09:04:28 -0400

    changing colors

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { HttpClientModule } from '@angular/common/http';
+import {MatButtonModule} from '@angular/material/button'; 
 
 @NgModule({
   declarations: [
@@ -14,6 +15,7 @@ import { HttpClientModule } from '@angular/common/http';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    MatButtonModule
   ],
   providers: [
     provideAnimationsAsync()
